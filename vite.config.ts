4a0c26faa743b096e318d40b953126012f7c5080
shirayuki <shirayuki@46ki75.com>
2024-10-30 02:22:13 +0900

    chore(vite): update build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,13 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import dts from 'vite-plugin-dts'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), dts()],
+  plugins: [vue(), vueJsx(), dts(), cssInjectedByJsPlugin()],
   base: '/elmethis',
   build: {
     lib: {
