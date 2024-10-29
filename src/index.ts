05e7d33aebf0673541dda8360001c725c3003551
shirayuki <shirayuki@46ki75.com>
2024-10-30 02:00:38 +0900

    chore(vite): update build configuration

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+// icon
+import ElmDotLoadingIcon from './components/icon/ElmDotLoadingIcon.vue'
+import ElmToggleTheme from './components/icon/ElmToggleTheme.vue'
+
+export { ElmDotLoadingIcon, ElmToggleTheme }
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,29 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import dts from 'vite-plugin-dts'
+
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [vue(), vueJsx(), dts()],
+  base: '/elmethis',
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'elmethis',
+      fileName: 'elmethis'
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  },
   esbuild: {
     jsx: 'preserve'
   },
