e5985b6d30f6b6854ed12c3372fe8f0c675d8e63
shirayuki <shirayuki@46ki75.com>
2024-10-31 04:47:15 +0900

    feat(index): add new components and reorganize imports for better structure

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,22 @@
 // code
 import ElmPrismHighlighter from './components/code/ElmPrismHighlighter.vue'
 
+// containments
+import ElmTooltip from './components/containments/ElmTooltip.vue'
+
+// fallback
+import ElmRectangleWave from './components/fallback/ElmRectangleWave.vue'
+
+// Headings
+import ElmHeading1 from './components/headings/ElmHeading1.vue'
+import ElmHeading2 from './components/headings/ElmHeading2.vue'
+import ElmHeading3 from './components/headings/ElmHeading3.vue'
+import ElmHeading4 from './components/headings/ElmHeading4.vue'
+import ElmHeading5 from './components/headings/ElmHeading5.vue'
+import ElmHeading6 from './components/headings/ElmHeading6.vue'
+
 // icon
+import ElmCubeIcon from './components/icon/ElmCubeIcon.vue'
 import ElmDotLoadingIcon from './components/icon/ElmDotLoadingIcon.vue'
 import ElmToggleTheme from './components/icon/ElmToggleTheme.vue'
 
@@ -11,28 +26,35 @@ import ElmInlineLink from './components/inline/ElmInlineLink.vue'
 import ElmInlineRuby from './components/inline/ElmInlineRuby.vue'
 import ElmInlineText from './components/inline/ElmInlineText.vue'
 
+// media
+import ElmImage from './components/media/ElmImage.vue'
+
 // typography
+import ElmBulletedList from './components/typography/ElmBulletedList.vue'
 import ElmCallout from './components/typography/ElmCallout.vue'
-import ElmHeading1 from './components/headings/ElmHeading1.vue'
-import ElmHeading2 from './components/headings/ElmHeading2.vue'
-import ElmHeading3 from './components/headings/ElmHeading3.vue'
-import ElmHeading4 from './components/headings/ElmHeading4.vue'
-import ElmHeading5 from './components/headings/ElmHeading5.vue'
-import ElmHeading6 from './components/headings/ElmHeading6.vue'
+import ElmDivider from './components/typography/ElmDivider.vue'
+import ElmNumberedList from './components/typography/ElmNumberedList.vue'
 
 export {
   ElmPrismHighlighter,
+  ElmTooltip,
+  ElmRectangleWave,
+  ElmHeading1,
+  ElmHeading2,
+  ElmHeading3,
+  ElmHeading4,
+  ElmHeading5,
+  ElmHeading6,
+  ElmCubeIcon,
   ElmDotLoadingIcon,
   ElmToggleTheme,
   ElmInlineCode,
   ElmInlineLink,
   ElmInlineRuby,
   ElmInlineText,
+  ElmImage,
+  ElmBulletedList,
   ElmCallout,
-  ElmHeading1,
-  ElmHeading2,
-  ElmHeading3,
-  ElmHeading4,
-  ElmHeading5,
-  ElmHeading6
+  ElmDivider,
+  ElmNumberedList
 }
