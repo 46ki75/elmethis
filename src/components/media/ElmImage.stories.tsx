6d54fe60063654357874e497e0c2c452ddbd530e
shirayuki <shirayuki@46ki75.com>
2024-10-31 00:48:55 +0900

    feat(image): create component

---
+++ src/components/media/ElmImage.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+import ElmImage from './ElmImage.vue'
+
+const meta: Meta<typeof ElmImage> = {
+  title: 'Components/Media/ElmImage',
+  component: ElmImage,
+  tags: ['autodocs'],
+  args: {
+    src: 'https://images.unsplash.com/photo-1556983703-27576e5afa24?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb'
+  }
+}
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {}
---
+++ src/components/media/ElmImage.vue
@@ -0,0 +1,39 @@
+<template>
+  <img
+    class="image"
+    :src="src"
+    :alt="alt"
+    @load="
+      () => {
+        isLoading = false
+      }
+    "
+  />
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+withDefaults(
+  defineProps<{
+    /**
+     * Image source URL
+     */
+    src: string
+
+    /**
+     * Image alt text
+     */
+    alt?: string
+  }>(),
+  {}
+)
+
+const isLoading = ref(true)
+</script>
+
+<style scoped lang="scss">
+.image {
+  width: 100%;
+}
+</style>
