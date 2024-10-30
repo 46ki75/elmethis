71cc1d719f79376ff2ae9db06358a1bf4edfcddf
shirayuki <shirayuki@46ki75.com>
2024-10-31 03:48:41 +0900

    feat(ElmBulletedList): add bulleted list component with nested list support

---
+++ src/components/typography/ElmBulletedList.stories.tsx
@@ -0,0 +1,47 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+import ElmBulletedList from './ElmBulletedList.vue'
+import ElmInlineText from '../inline/ElmInlineText.vue'
+
+const meta: Meta<typeof ElmBulletedList> = {
+  title: 'Components/Typography/ElmBulletedList',
+  component: ElmBulletedList,
+  tags: ['autodocs'],
+  args: {}
+}
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {
+  render: () => ({
+    components: { ElmBulletedList, ElmInlineText },
+    template: `<ElmBulletedList>
+      <li><ElmInlineText text='Item 1'/></li>
+      <li><ElmInlineText text='Item 2'/></li>
+      <li><ElmInlineText text='Item 3'/></li>
+    </ElmBulletedList>`
+  })
+}
+
+export const Nested: Story = {
+  render: () => ({
+    components: { ElmBulletedList, ElmInlineText },
+    template: `<ElmBulletedList>
+      <li><ElmInlineText text='Item 1'/></li>
+      <li><ElmInlineText text='Item 2'/>
+        <ElmBulletedList>
+          <li><ElmInlineText text='Item 2.1'/></li>
+          <li><ElmInlineText text='Item 2.2'/>
+            <ElmBulletedList>
+              <li><ElmInlineText text='Item 2.2.1'/></li>
+              <li><ElmInlineText text='Item 2.2.2'/></li>
+              <li><ElmInlineText text='Item 2.2.3'/></li>
+            </ElmBulletedList>
+          </li>
+          <li><ElmInlineText text='Item 2.3'/></li>
+        </ElmBulletedList>
+      </li>
+      <li><ElmInlineText text='Item 3'/></li>
+    </ElmBulletedList>`
+  })
+}
