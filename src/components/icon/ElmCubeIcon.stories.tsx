ff6f9c393d535e19f8c84b52768f773af34f7a2a
shirayuki <shirayuki@46ki75.com>
2024-10-31 01:25:38 +0900

    feat(ElmCubeIcon): add new 3D cube icon component with animations

---
+++ src/components/icon/ElmCubeIcon.stories.tsx
@@ -0,0 +1,14 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+import ElmCubeIcon from './ElmCubeIcon.vue'
+
+const meta: Meta<typeof ElmCubeIcon> = {
+  title: 'Components/Icon/ElmCubeIcon',
+  component: ElmCubeIcon,
+  tags: ['autodocs'],
+  args: {}
+}
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {}
---
+++ src/components/icon/ElmCubeIcon.vue
@@ -0,0 +1,88 @@
+<template>
+  <div
+    class="cube"
+    :style="{
+      width: `${size}px`,
+      height: `${size}px`
+    }"
+  >
+    <div
+      v-for="face in faces"
+      :key="face.name"
+      class="face"
+      :style="{
+        transform: `${face.rotate} ${commonTranslateZ}`
+      }"
+    ></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+
+// Default prop values
+const props = withDefaults(
+  defineProps<{
+    /**
+     * The size of the cube [px]
+     */
+    size?: number
+  }>(),
+  {
+    size: 128
+  }
+)
+
+const commonTranslateZ = computed(() => `translateZ(${props.size / 2}px)`)
+
+const faces = [
+  { name: 'front', rotate: '' },
+  { name: 'back', rotate: 'rotateY(180deg)' },
+  { name: 'left', rotate: 'rotateY(-90deg)' },
+  { name: 'right', rotate: 'rotateY(90deg)' },
+  { name: 'top', rotate: 'rotateX(90deg)' },
+  { name: 'bottom', rotate: 'rotateX(-90deg)' }
+]
+</script>
+
+<style scoped lang="scss">
+.cube {
+  position: relative;
+  transform-style: preserve-3d;
+  transform: rotateX(-30deg) rotateY(-45deg);
+  animation: elmethis-cube-icon-rotate 1500ms infinite linear;
+}
+
+.face {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  border-width: 1px;
+  border-style: solid;
+  border-radius: 2px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: bold;
+  backface-visibility: hidden;
+  user-select: none;
+
+  border-style: solid;
+  border-width: 1px;
+  border-color: rgba(0, 0, 0, 0.7);
+  background-color: rgba(255, 255, 255, 0.2);
+  [data-theme='dark'] & {
+    border-color: rgba(255, 255, 255, 0.7);
+    background-color: rgba(0, 0, 0, 0.2);
+  }
+}
+
+@keyframes elmethis-cube-icon-rotate {
+  from {
+    transform: rotateX(-30deg) rotateY(-45deg) rotateZ(0deg);
+  }
+  to {
+    transform: rotateX(-30deg) rotateY(315deg) rotateZ(360deg);
+  }
+}
+</style>
