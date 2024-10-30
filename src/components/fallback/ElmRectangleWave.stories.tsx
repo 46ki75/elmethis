b4a91f82468172d748259bf38b5dd7f902122ac5
shirayuki <shirayuki@46ki75.com>
2024-10-31 01:04:52 +0900

    feat(ElmRectangleWave): add new fallback component with animations

---
+++ src/components/fallback/ElmRectangleWave.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+import ElmRectangleWave from './ElmRectangleWave.vue'
+
+const meta: Meta<typeof ElmRectangleWave> = {
+  title: 'Components/Fallback/ElmRectangleWave',
+  component: ElmRectangleWave,
+  tags: ['autodocs'],
+  args: {},
+  render: () => {
+    return {
+      components: { ElmRectangleWave },
+      template: `<div style="height: 400px;"><ElmRectangleWave /></div>`
+    }
+  }
+}
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {}
---
+++ src/components/fallback/ElmRectangleWave.vue
@@ -0,0 +1,47 @@
+<template>
+  <div aria-hidden class="rectangle-wave"></div>
+</template>
+
+<style scoped lang="scss">
+@keyframes wave {
+  0% {
+    transform: scale(0);
+    opacity: 0;
+  }
+
+  30% {
+    opacity: 0;
+  }
+
+  40% {
+    opacity: 1;
+  }
+
+  100% {
+    transform: scale(1);
+    opacity: 0;
+  }
+}
+
+.rectangle-wave {
+  box-sizing: border-box;
+  position: absolute;
+  overflow: hidden;
+
+  width: 100%;
+  height: 100%;
+
+  animation-name: wave;
+  animation-duration: 1200ms;
+  animation-iteration-count: infinite;
+  animation-fill-mode: both;
+
+  border-style: solid;
+  border-width: 1px;
+  border-color: rgba(0, 0, 0, 0.7);
+
+  [data-theme='dark'] & {
+    border-color: rgba(255, 255, 255, 0.7);
+  }
+}
+</style>
