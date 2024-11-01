088fa2950742214b36277e60b3abf48ea055b7e6
shirayuki <shirayuki@46ki75.com>
2024-11-01 09:37:47 +0900

    feat(ElmTableBody): add custom styles and transition effects to table body

--- src/components/table/ElmTableBody.vue
+++ src/components/table/ElmTableBody.vue
@@ -1,5 +1,5 @@
 <template>
-  <tbody>
+  <tbody class="elmethis-table-header-body">
     <slot />
   </tbody>
 </template>
@@ -10,4 +10,16 @@ export interface ElmTableBodyProps {}
 withDefaults(defineProps<ElmTableBodyProps>(), {})
 </script>
 
-<style scoped lang="scss"></style>
+<style lang="scss">
+.elmethis-table-header-body {
+  background-color: rgba(black, 0.05);
+  color: rgba(black, 0.7);
+  transition:
+    background-color 400ms,
+    color 400ms;
+  [data-theme='dark'] & {
+    background-color: rgba(white, 0.05);
+    color: rgba(white, 0.7);
+  }
+}
+</style>
