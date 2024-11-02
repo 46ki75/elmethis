216bd238c45f621afab1c24d8672e2e6454503ea
shirayuki <shirayuki@46ki75.com>
2024-11-02 17:22:25 +0900

    feat: enhance ElmFile component with download icon and improved styling

--- packages/core/src/components/media/ElmFile.vue
+++ packages/core/src/components/media/ElmFile.vue
@@ -4,11 +4,15 @@
       <DocumentIcon class="icon" />
       <ElmInlineText :text="name" />
     </div>
+
+    <div class="right-container">
+      <ArrowDownTrayIcon class="download-icon" />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { DocumentIcon } from '@heroicons/vue/24/outline'
+import { ArrowDownTrayIcon, DocumentIcon } from '@heroicons/vue/24/outline'
 import ElmInlineText from '../inline/ElmInlineText.vue'
 
 export interface ElmFileProps {
@@ -29,19 +33,51 @@ withDefaults(defineProps<ElmFileProps>(), {})
   justify-content: space-between;
   align-items: center;
 
+  background-color: rgba(white, 0.2);
+  [data-theme='dark'] & {
+    background-color: rgba(black, 0.2);
+  }
+
   .left-container {
     display: flex;
     justify-content: flex-start;
     align-items: center;
     gap: 0.75rem;
+
+    .icon {
+      width: 20px;
+      transition: color 200ms;
+      color: rgba(black, 0.8);
+      [data-theme='dark'] & {
+        color: rgba(white, 0.8);
+      }
+    }
   }
 
-  .icon {
-    width: 20px;
-    transition: color 200ms;
-    color: rgba(black, 0.8);
-    [data-theme='dark'] & {
-      color: rgba(white, 0.8);
+  .right-container {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    gap: 0.75rem;
+
+    .download-icon {
+      padding: 0.125rem;
+      width: 20px;
+      cursor: pointer;
+      transition:
+        color 200ms,
+        background-color 200ms;
+      color: rgba(black, 0.8);
+      [data-theme='dark'] & {
+        color: rgba(white, 0.8);
+      }
+
+      &:hover {
+        background-color: rgba(black, 0.1);
+        [data-theme='dark'] & {
+          background-color: rgba(white, 0.1);
+        }
+      }
     }
   }
 }
