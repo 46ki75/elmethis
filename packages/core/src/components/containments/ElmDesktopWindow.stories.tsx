ec4d04108c04d87f60d4c06f4f4f5dbe0c6443df
shirayuki <shirayuki@46ki75.com>
2024-11-03 22:15:54 +0900

    feat: add ElmDesktopWindow component with Storybook integration

---
+++ packages/core/src/components/containments/ElmDesktopWindow.stories.tsx
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+import ElmDesktopWindow from './ElmDesktopWindow.vue'
+import ElmParagraph from '../typography/ElmParagraph.vue'
+
+const meta: Meta<typeof ElmDesktopWindow> = {
+  title: 'Components/Containments/ElmDesktopWindow',
+  component: ElmDesktopWindow,
+  tags: ['autodocs'],
+  args: {}
+}
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {
+  render: () => ({
+    components: { ElmDesktopWindow, ElmParagraph },
+    template: `<ElmDesktopWindow>
+      <div style="padding: 1rem;">
+        <ElmParagraph>
+          This is the content of the desktop window.
+        </ElmParagraph>
+      </div>
+    </ElmDesktopWindow>`
+  })
+}
---
+++ packages/core/src/components/containments/ElmDesktopWindow.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="window">
+    <div class="header">
+      <div aria-hidden class="dot" :style="{ '--bg': '#c48691' }"></div>
+      <div aria-hidden class="dot" :style="{ '--bg': '#c9b990' }"></div>
+      <div aria-hidden class="dot" :style="{ '--bg': '#7cc598' }"></div>
+    </div>
+    <div class="body"><slot /></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+export interface ElmDesktopWindowProps {}
+
+withDefaults(defineProps<ElmDesktopWindowProps>(), {})
+</script>
+
+<style scoped lang="scss">
+.window {
+  min-height: 100px;
+  border-radius: 0.25rem;
+  overflow: hidden;
+  box-shadow: 0 0 0.25rem rgba(black, 0.15);
+  background-color: rgba(black, 0.05);
+  [data-theme='dark'] & {
+    box-shadow: 0 0 0.25rem rgba(black, 0.35);
+    background-color: rgba(white, 0.05);
+  }
+
+  .header {
+    box-sizing: border-box;
+    padding: 0.5rem 0.75rem;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    gap: 0.5rem;
+    height: 2rem;
+    background-color: rgba(black, 0.1);
+
+    .dot {
+      width: 0.75rem;
+      height: 0.75rem;
+      border-radius: 50%;
+      background-color: var(--bg);
+    }
+  }
+}
+</style>
