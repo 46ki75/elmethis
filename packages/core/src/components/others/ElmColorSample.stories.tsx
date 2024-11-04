24fc1f9c3a0a4da9eaabdfb351e03991008a9ee2
shirayuki <shirayuki@46ki75.com>
2024-11-04 11:40:34 +0900

    feat: add ElmColorSample component with tooltip for color display

---
+++ packages/core/src/components/others/ElmColorSample.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+import ElmColorSample from './ElmColorSample.vue'
+
+const meta: Meta<typeof ElmColorSample> = {
+  title: 'Components/Others/ElmColorSample',
+  component: ElmColorSample,
+  tags: ['autodocs'],
+  args: {}
+}
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {
+  args: {
+    color: '#59b57c'
+  }
+}
---
+++ packages/core/src/components/others/ElmColorSample.vue
@@ -0,0 +1,60 @@
+<template>
+  <div
+    :style="{
+      '--color': color
+    }"
+  >
+    <ElmTooltip>
+      <template #original>
+        <div
+          class="color-bg"
+          :style="{
+            '--background-color': color
+          }"
+        ></div>
+        <div class="text">{{ rgbToColorString(parseToRgb(color)) }}</div>
+      </template>
+      <template #tooltip>
+        <div class="text">{{ rgbToColorString(parseToRgb(color)) }}</div>
+        <div class="text">{{ `rgb(${red}, ${green}, ${blue})` }}</div>
+        <div class="text">
+          {{
+            `hsl(${Math.floor(hue)}, ${Math.floor(saturation * 100)}%, ${Math.floor(lightness * 100)}%)`
+          }}
+        </div>
+      </template>
+    </ElmTooltip>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { parseToHsl, parseToRgb, rgbToColorString } from 'polished'
+import ElmTooltip from '../containments/ElmTooltip.vue'
+
+export interface ElmColorSampleProps {
+  color: string
+}
+
+const props = withDefaults(defineProps<ElmColorSampleProps>(), {})
+
+const { blue, green, red } = parseToRgb(props.color)
+const { hue, saturation, lightness } = parseToHsl(props.color)
+</script>
+
+<style scoped lang="scss">
+.color-bg {
+  width: 5rem;
+  height: 2rem;
+  border-radius: 0.25rem;
+  background-color: var(--background-color);
+  cursor: pointer;
+}
+
+.text {
+  color: var(--color);
+  &::selection {
+    color: white;
+    background-color: var(--color);
+  }
+}
+</style>
