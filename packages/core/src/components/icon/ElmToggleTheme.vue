f052ba3bc824e0803797481afe4b73992ed4c8c3
shirayuki <shirayuki@46ki75.com>
2024-11-08 02:18:20 +0900

    feat: integrate Iconify for theme toggle icons in ElmToggleTheme component

--- packages/core/src/components/icon/ElmToggleTheme.vue
+++ packages/core/src/components/icon/ElmToggleTheme.vue
@@ -1,16 +1,24 @@
 <template>
-  <component
+  <Icon
+    v-if="!isDarkTheme"
+    icon="line-md:moon-to-sunny-outline-loop-transition"
     :class="$style.icon"
-    :is="isDarkTheme ? MoonIcon : SunIcon"
-    :style="{ width: size, height: size }"
+    :width="size"
+    @click="toggleTheme"
+  />
+  <Icon
+    v-else
+    icon="line-md:sunny-outline-to-moon-loop-transition"
+    :class="$style.icon"
+    :width="size"
     @click="toggleTheme"
   />
 </template>
 
 <script setup lang="ts">
-import { MoonIcon, SunIcon } from '@heroicons/vue/24/outline'
 import type { Property } from 'csstype'
 import { useElmethisTheme } from '../../hooks/useElmethisTheme'
+import { Icon } from '@iconify/vue'
 
 export interface ElmToggleThemeProps {
   /**
@@ -32,13 +40,16 @@ const { isDarkTheme, toggleTheme } = useElmethisTheme()
   padding: 0.25rem;
   color: $color;
   border-radius: 50%;
-  box-shadow: 0 0 0.125rem rgba(128, 128, 128, 0.6);
   cursor: pointer;
 }
 
 .icon {
   @include icon(rgba(0, 0, 0, 0.8));
+  box-shadow: 0 0 0.125rem rgba(black, 0.3);
+  background-color: rgba(white, 0.2);
   [data-theme='dark'] & {
+    box-shadow: 0 0 0.125rem rgba(black, 0.6);
+    background-color: rgba(black, 0.2);
     @include icon(rgba(255, 255, 255, 0.8));
   }
 }
