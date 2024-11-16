16d0b19fc47ba580d80123e811599ab6f6eb3a38
shirayuki <shirayuki@46ki75.com>
2024-11-16 21:41:31 +0900

    feat(jsonschema): add ElmFieldType component with type representation and styling

---
+++ packages/core/src/components/jsonschema/ElmFieldType.stories.tsx
@@ -0,0 +1,41 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+import ElmFieldType from './ElmFieldType.vue'
+
+const meta: Meta<typeof ElmFieldType> = {
+  title: 'Components/JSONSchema/ElmFieldType',
+  component: ElmFieldType,
+  tags: ['autodocs'],
+  args: {},
+  argTypes: {
+    type: {
+      options: [
+        'object',
+        'array',
+        'string',
+        'number',
+        'boolean',
+        'integer',
+        'null'
+      ],
+      control: { type: 'radio' }
+    }
+  }
+}
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {
+  args: {
+    type: 'object',
+    name: 'user'
+  }
+}
+
+export const Maybe: Story = {
+  args: {
+    type: 'object',
+    name: 'user',
+    nullable: true
+  }
+}
