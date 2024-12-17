362015f8a3a9dd8cd7419d429246045052b69c68
shirayuki <shirayuki@46ki75.com>
2024-12-17 19:18:27 +0900

    feat: add ElmTextField component with basic structure and styles

---
+++ packages/core/src/components/form/ElmTextField.stories.tsx
@@ -0,0 +1,14 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+import ElmTextField from './ElmTextField.vue'
+
+const meta: Meta<typeof ElmTextField> = {
+  title: 'Components/Form/ElmTextField',
+  component: ElmTextField,
+  tags: ['autodocs'],
+  args: {}
+}
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {}
---
+++ packages/core/src/components/form/ElmTextField.vue
@@ -0,0 +1,32 @@
+<template>
+  <label :class="$style.wrapper">
+    <ElmInlineText text="Label" />
+    <input type="text" />
+  </label>
+</template>
+
+<script setup lang="ts">
+import ElmInlineText from '../inline/ElmInlineText.vue'
+
+export interface ElmTextFieldProps {}
+
+withDefaults(defineProps<ElmTextFieldProps>(), {})
+</script>
+
+<style module lang="scss">
+.wrapper {
+  position: relative;
+}
+
+.label {
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+
+.input {
+  all: unset;
+  box-sizing: border-box;
+  padding: 0.5rem;
+}
+</style>
