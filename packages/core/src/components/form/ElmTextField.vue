00634feff75681505b5a00775a7d60d467c6dbf1
shirayuki <shirayuki@46ki75.com>
2024-12-17 20:01:17 +0900

    feat: enhance ElmTextField styles and structure for improved usability

--- packages/core/src/components/form/ElmTextField.vue
+++ packages/core/src/components/form/ElmTextField.vue
@@ -1,7 +1,7 @@
 <template>
   <label :class="$style.wrapper">
-    <ElmInlineText text="Label" />
-    <input type="text" />
+    <ElmInlineText text="Label" :class="$style.label" />
+    <input type="text" :class="$style.input" />
   </label>
 </template>
 
@@ -20,13 +20,22 @@ withDefaults(defineProps<ElmTextFieldProps>(), {})
 
 .label {
   position: absolute;
-  top: 0;
-  left: 0;
+  top: -100%;
+  left: 0.25rem;
+  padding: 0.25rem;
+  font-size: 0.75rem;
+  color: rgba(black, 0.6);
 }
 
 .input {
   all: unset;
   box-sizing: border-box;
-  padding: 0.5rem;
+  padding: 0.5rem 0.75rem;
+  padding-top: 1.5rem;
+  border-radius: 0.25rem;
+  background-color: rgba(white, 0.8);
+  color: rgba(black, 0.7);
+  box-shadow: 0 0 0.25rem rgba(black, 0.1);
+  cursor: text;
 }
 </style>
