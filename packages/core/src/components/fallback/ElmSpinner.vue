7b7e9bb5298be1d8ef82fc03721087a23ce60f22
Shirayuki Chomolungma <shirayuki@46ki75.com>
2025-01-20 20:55:30 +0900

    feat: add ElmSpinner component with customizable radius and weight

---
+++ packages/core/src/components/fallback/ElmSpinner.vue
@@ -0,0 +1,57 @@
+<template>
+  <svg :height="radius * 2" :width="radius * 2">
+    <circle
+      :class="$style.circle"
+      :cx="radius"
+      :cy="radius"
+      :r="radius - weight"
+      fill="transparent"
+      :strokeWidth="weight"
+    />
+  </svg>
+</template>
+
+<script setup lang="ts">
+export interface ElmSpinnerProps {
+  radius?: number
+  weight?: number
+}
+
+withDefaults(defineProps<ElmSpinnerProps>(), {
+  radius: 16,
+  weight: 2
+})
+</script>
+
+<style module lang="scss">
+@keyframes draw-line {
+  0% {
+    stroke-dashoffset: 0%;
+  }
+  100% {
+    stroke-dashoffset: 400%;
+  }
+}
+
+@keyframes rotate {
+  0% {
+    transform: rotate(180deg);
+  }
+  100% {
+    transform: rotate(-180deg);
+  }
+}
+
+.circle {
+  stroke-dasharray: 200%;
+  animation:
+    draw-line 1200ms infinite ease-in-out,
+    rotate 1200ms infinite linear;
+  transform-origin: center;
+
+  stroke: rgba(black, 0.7);
+  [data-theme='dark'] & {
+    stroke: rgba(white, 0.7);
+  }
+}
+</style>
