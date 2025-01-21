a3b6b03c03988ca76aa104243d885449e3b18c75
Shirayuki Chomolungma <shirayuki@46ki75.com>
2025-01-22 00:10:23 +0900

    chore: bump version to 1.0.0-alpha.121 and update vite configuration

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 import dts from 'vite-plugin-dts'
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
@@ -9,6 +10,7 @@ import { resolve } from 'path'
 export default defineConfig({
   plugins: [
     vue(),
+    vueJsx(),
     dts({ tsconfigPath: './tsconfig.app.json' }),
     cssInjectedByJsPlugin()
   ],
