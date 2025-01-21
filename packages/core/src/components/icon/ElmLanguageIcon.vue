e241730e906a8e051475d70d488497db3e2c9644
Shirayuki Chomolungma <shirayuki@46ki75.com>
2025-01-22 01:16:51 +0900

    chore: bump version to 1.0.0-alpha.131 and update ElmLanguageIcon to use Iconify

--- packages/core/src/components/icon/ElmLanguageIcon.vue
+++ packages/core/src/components/icon/ElmLanguageIcon.vue
@@ -7,9 +7,8 @@
 </template>
 
 <script setup lang="ts">
-import { defineAsyncComponent, ref } from 'vue'
-
-import { CommandLineIcon } from '@heroicons/vue/24/outline'
+import { defineAsyncComponent, h, ref, useCssModule } from 'vue'
+import { Icon as Iconify } from '@iconify/vue'
 import { watch } from 'vue'
 
 export interface ElmLanguageIconProps {
@@ -28,27 +27,30 @@ const props = withDefaults(defineProps<ElmLanguageIconProps>(), {
   size: 24
 })
 
+const style = useCssModule()
+const Fallback = h(Iconify, { icon: 'mdi:terminal-line', class: style.icon })
+
 const render = () => {
   switch (props.language.toLowerCase()) {
     case 'rust':
     case 'rs':
       return defineAsyncComponent({
         loader: () => import('./languages/Rust.vue'),
-        loadingComponent: CommandLineIcon
+        loadingComponent: Fallback
       })
 
     case 'javascript':
     case 'js':
       return defineAsyncComponent({
         loader: () => import('./languages/JavaScript.vue'),
-        loadingComponent: CommandLineIcon
+        loadingComponent: Fallback
       })
 
     case 'typescript':
     case 'ts':
       return defineAsyncComponent({
         loader: () => import('./languages/TypeScript.vue'),
-        loadingComponent: CommandLineIcon
+        loadingComponent: Fallback
       })
 
     case 'bash':
@@ -56,7 +58,7 @@ const render = () => {
     case 'shell':
       return defineAsyncComponent({
         loader: () => import('./languages/Bash.vue'),
-        loadingComponent: CommandLineIcon
+        loadingComponent: Fallback
       })
 
     case 'tf':
@@ -64,94 +66,94 @@ const render = () => {
     case 'hcl':
       return defineAsyncComponent({
         loader: () => import('./languages/Terraform.vue'),
-        loadingComponent: CommandLineIcon
+        loadingComponent: Fallback
       })
 
     case 'html':
       return defineAsyncComponent({
         loader: () => import('./languages/Html.vue'),
-        loadingComponent: CommandLineIcon
+        loadingComponent: Fallback
       })
 
     case 'css':
       return defineAsyncComponent({
         loader: () => import('./languages/Css.vue'),
-        loadingComponent: CommandLineIcon
+        loadingComponent: Fallback
       })
 
     case 'npm':
       return defineAsyncComponent({
         loader: () => import('./languages/Npm.vue'),
-        loadingComponent: CommandLineIcon
+        loadingComponent: Fallback
       })
 
     case 'java':
       return defineAsyncComponent({
         loader: () => import('./languages/Java.vue'),
-        loadingComponent: CommandLineIcon
+        loadingComponent: Fallback
       })
 
     case 'kotlin':
     case 'kt':
       return defineAsyncComponent({
         loader: () => import('./languages/Kotlin.vue'),
-        loadingComponent: CommandLineIcon
+        loadingComponent: Fallback
       })
 
     case 'go':
     case 'golang':
       return defineAsyncComponent({
         loader: () => import('./languages/Go.vue'),
-        loadingComponent: CommandLineIcon
+        loadingComponent: Fallback
       })
 
     case 'python':
     case 'py':
       return defineAsyncComponent({
         loader: () => import('./languages/Python.vue'),
-        loadingComponent: CommandLineIcon
+        loadingComponent: Fallback
       })
 
     case 'sql':
       return defineAsyncComponent({
         loader: () => import('./languages/Sql.vue'),
-        loadingComponent: CommandLineIcon
+        loadingComponent: Fallback
       })
 
     case 'json':
       return defineAsyncComponent({
         loader: () => import('./languages/Json.vue'),
-        loadingComponent: CommandLineIcon
+        loadingComponent: Fallback
       })
 
     case 'lua':
       return defineAsyncComponent({
         loader: () => import('./languages/Lua.vue'),
-        loadingComponent: CommandLineIcon
+        loadingComponent: Fallback
       })
 
     case 'cs':
     case 'csharp':
       return defineAsyncComponent({
         loader: () => import('./languages/Csharp.vue'),
-        loadingComponent: CommandLineIcon
+        loadingComponent: Fallback
       })
 
     case 'cpp':
     case 'c++':
       return defineAsyncComponent({
         loader: () => import('./languages/Cpp.vue'),
-        loadingComponent: CommandLineIcon
+        loadingComponent: Fallback
       })
 
     case 'c':
       return defineAsyncComponent({
         loader: () => import('./languages/C.vue'),
-        loadingComponent: CommandLineIcon
+        loadingComponent: Fallback
       })
 
     default:
-      return CommandLineIcon
+      return Fallback
   }
 }
 
