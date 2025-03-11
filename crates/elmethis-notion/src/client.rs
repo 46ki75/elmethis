39857561a0a97a8404a3fb8e59515f09002bb445
Shirayuki Chomolungma <shirayuki@46ki75.com>
2025-03-11 12:48:02 +0000

    chore(deps): update dependencies and bump edition to 2024

--- crates/elmethis-notion/src/client.rs
+++ crates/elmethis-notion/src/client.rs
@@ -1,6 +1,6 @@
 #[derive(Debug)]
 pub struct Client {
-    pub notionrs_client: notionrs::Client,
+    pub notionrs_client: notionrs::client::Client,
     pub images: Vec<Image>,
 }
 
@@ -16,7 +16,7 @@ impl Client {
         T: AsRef<str>,
     {
         Client {
-            notionrs_client: notionrs::Client::new().secret(secret),
+            notionrs_client: notionrs::client::Client::new().secret(secret),
             images: Vec::default(),
         }
     }
@@ -37,8 +37,8 @@ impl Client {
 
         for block in results {
             match block.block {
-                notionrs::block::Block::Audio { audio: _ } => {}
-                notionrs::block::Block::Bookmark { bookmark } => {
+                notionrs::object::block::Block::Audio { audio: _ } => {}
+                notionrs::object::block::Block::Bookmark { bookmark } => {
                     let mut props = crate::block::ElmBookmarkProps {
                         url: bookmark.url.clone(),
                         margin: "2rem".to_string(),
@@ -107,8 +107,8 @@ impl Client {
 
                     blocks.push(block);
                 }
-                notionrs::block::Block::Breadcrumb { breadcrumb: _ } => {}
-                notionrs::block::Block::BulletedListItem { bulleted_list_item } => {
+                notionrs::object::block::Block::Breadcrumb { breadcrumb: _ } => {}
+                notionrs::object::block::Block::BulletedListItem { bulleted_list_item } => {
                     let mut list_item_children: Vec<crate::block::Block> = Vec::new();
 
                     let rich_text_block = Client::convert_rich_text(bulleted_list_item.rich_text);
@@ -138,7 +138,7 @@ impl Client {
                         }
                     };
                 }
-                notionrs::block::Block::Callout { callout } => {
+                notionrs::object::block::Block::Callout { callout } => {
                     let mut children: Vec<crate::block::Block> = Vec::new();
                     let text_blocks = Client::convert_rich_text(callout.rich_text);
                     let children_blocks = self.convert_block(&block.id).await?;
@@ -146,24 +146,26 @@ impl Client {
                     children.extend(children_blocks);
 
                     let r#type = match callout.color {
-                        notionrs::Color::Default
-                        | notionrs::Color::DefaultBackground
-                        | notionrs::Color::Blue
-                        | notionrs::Color::BlueBackground
-                        | notionrs::Color::Gray
-                        | notionrs::Color::GrayBackground => "note",
-                        notionrs::Color::Green | notionrs::Color::GreenBackground => "tip",
-                        notionrs::Color::Purple | notionrs::Color::PurpleBackground => "important",
-                        notionrs::Color::Yellow
-                        | notionrs::Color::YellowBackground
-                        | notionrs::Color::Orange
-                        | notionrs::Color::OrangeBackground
-                        | notionrs::Color::Brown
-                        | notionrs::Color::BrownBackground => "warning",
-                        notionrs::Color::Red
-                        | notionrs::Color::RedBackground
-                        | notionrs::Color::Pink
-                        | notionrs::Color::PinkBackground => "caution",
+                        notionrs::object::color::Color::Default
+                        | notionrs::object::color::Color::DefaultBackground
+                        | notionrs::object::color::Color::Blue
+                        | notionrs::object::color::Color::BlueBackground
+                        | notionrs::object::color::Color::Gray
+                        | notionrs::object::color::Color::GrayBackground => "note",
+                        notionrs::object::color::Color::Green
+                        | notionrs::object::color::Color::GreenBackground => "tip",
+                        notionrs::object::color::Color::Purple
+                        | notionrs::object::color::Color::PurpleBackground => "important",
+                        notionrs::object::color::Color::Yellow
+                        | notionrs::object::color::Color::YellowBackground
+                        | notionrs::object::color::Color::Orange
+                        | notionrs::object::color::Color::OrangeBackground
+                        | notionrs::object::color::Color::Brown
+                        | notionrs::object::color::Color::BrownBackground => "warning",
+                        notionrs::object::color::Color::Red
+                        | notionrs::object::color::Color::RedBackground
+                        | notionrs::object::color::Color::Pink
+                        | notionrs::object::color::Color::PinkBackground => "caution",
                     }
                     .to_string();
 
@@ -175,9 +177,9 @@ impl Client {
                     });
                     blocks.push(callout_block);
                 }
-                notionrs::block::Block::ChildDatabase { child_database: _ } => {}
-                notionrs::block::Block::ChildPage { child_page: _ } => {}
-                notionrs::block::Block::Code { code } => {
+                notionrs::object::block::Block::ChildDatabase { child_database: _ } => {}
+                notionrs::object::block::Block::ChildPage { child_page: _ } => {}
+                notionrs::object::block::Block::Code { code } => {
                     let language = code.language.to_string();
 
                     let caption = code
@@ -205,28 +207,28 @@ impl Client {
                         crate::block::ElmCodeBlock { props },
                     ));
                 }
-                notionrs::block::Block::ColumnList { column_list: _ } => {
+                notionrs::object::block::Block::ColumnList { column_list: _ } => {
                     let children = self.convert_block(&block.id).await?;
                     let block = crate::block::Block::ElmColumnList(crate::block::ElmColumnList {
                         children,
                     });
                     blocks.push(block);
                 }
-                notionrs::block::Block::Column { column: _ } => {
+                notionrs::object::block::Block::Column { column: _ } => {
                     let children = self.convert_block(&block.id).await?;
                     let block =
                         crate::block::Block::ElmColumn(crate::block::ElmColumn { children });
                     blocks.push(block);
                 }
-                notionrs::block::Block::Divider { divider: _ } => {
+                notionrs::object::block::Block::Divider { divider: _ } => {
                     blocks.push(crate::block::Block::ElmDivider(crate::block::ElmDivider {
                         props: crate::block::ElmDividerProps {
                             margin: "2rem".to_string(),
                         },
                     }));
                 }
-                notionrs::block::Block::Embed { embed: _ } => {}
-                notionrs::block::Block::Equation { equation } => {
+                notionrs::object::block::Block::Embed { embed: _ } => {}
+                notionrs::object::block::Block::Equation { equation } => {
                     let props = crate::block::ElmKatexProps {
                         expression: equation.expression,
                         block: true,
@@ -236,10 +238,10 @@ impl Client {
 
                     blocks.push(block);
                 }
-                notionrs::block::Block::File { file } => {
+                notionrs::object::block::Block::File { file } => {
                     let (name, src) = match file {
-                        notionrs::File::External(f) => (f.name, f.external.url),
-                        notionrs::File::Uploaded(f) => (f.name, f.file.url),
+                        notionrs::object::file::File::External(f) => (f.name, f.external.url),
+                        notionrs::object::file::File::Uploaded(f) => (f.name, f.file.url),
                     };
 
                     let props = crate::block::ElmFileProps {
@@ -252,7 +254,7 @@ impl Client {
 
                     blocks.push(block);
                 }
-                notionrs::block::Block::Heading1 { heading_1 } => {
+                notionrs::object::block::Block::Heading1 { heading_1 } => {
                     let heading = heading_1;
                     if heading.is_toggleable {
                         let summary = heading
@@ -289,7 +291,7 @@ impl Client {
                         blocks.push(block);
                     }
                 }
-                notionrs::block::Block::Heading2 { heading_2 } => {
+                notionrs::object::block::Block::Heading2 { heading_2 } => {
                     let heading = heading_2;
                     if heading.is_toggleable {
                         let summary = heading
@@ -326,7 +328,7 @@ impl Client {
                         blocks.push(block);
                     }
                 }
-                notionrs::block::Block::Heading3 { heading_3 } => {
+                notionrs::object::block::Block::Heading3 { heading_3 } => {
                     let heading = heading_3;
                     if heading.is_toggleable {
                         let summary = heading
@@ -363,15 +365,15 @@ impl Client {
                         blocks.push(block);
                     }
                 }
-                notionrs::block::Block::Image { image } => {
+                notionrs::object::block::Block::Image { image } => {
                     let (src, alt) = match image {
-                        notionrs::File::External(f) => (
+                        notionrs::object::file::File::External(f) => (
                             f.external.url,
                             f.caption.map(|rich_text| {
                                 rich_text.iter().map(|t| t.to_string()).collect::<String>()
                             }),
                         ),
-                        notionrs::File::Uploaded(f) => (
+                        notionrs::object::file::File::Uploaded(f) => (
                             f.file.url,
                             f.caption.map(|rich_text| {
                                 rich_text.iter().map(|t| t.to_string()).collect::<String>()
@@ -392,8 +394,8 @@ impl Client {
                     blocks.push(image_block);
                     self.images.push(Image { src, id: block.id });
                 }
-                notionrs::block::Block::LinkPreview { link_preview: _ } => {}
-                notionrs::block::Block::NumberedListItem { numbered_list_item } => {
+                notionrs::object::block::Block::LinkPreview { link_preview: _ } => {}
+                notionrs::object::block::Block::NumberedListItem { numbered_list_item } => {
                     let mut list_item_children: Vec<crate::block::Block> = Vec::new();
 
                     let rich_text_block = Client::convert_rich_text(numbered_list_item.rich_text);
@@ -423,15 +425,15 @@ impl Client {
                         }
                     };
                 }
-                notionrs::block::Block::Paragraph { paragraph } => {
+                notionrs::object::block::Block::Paragraph { paragraph } => {
                     let block = crate::block::Block::ElmParagraph(crate::block::ElmParagraph {
                         children: Client::convert_rich_text(paragraph.rich_text),
                     });
 
                     blocks.push(block);
                 }
-                notionrs::block::Block::Pdf { pdf: _ } => {}
-                notionrs::block::Block::Quote { quote } => {
+                notionrs::object::block::Block::Pdf { pdf: _ } => {}
+                notionrs::object::block::Block::Quote { quote } => {
                     let mut children = Vec::new();
 
                     let inline_text_block = Client::convert_rich_text(quote.rich_text);
@@ -447,11 +449,11 @@ impl Client {
 
                     blocks.push(block);
                 }
-                notionrs::block::Block::SyncedBlock { synced_block: _ } => {
+                notionrs::object::block::Block::SyncedBlock { synced_block: _ } => {
                     let children = self.convert_block(&block.id).await?;
                     blocks.extend(children);
                 }
-                notionrs::block::Block::Table { table: _ } => {
+                notionrs::object::block::Block::Table { table: _ } => {
                     let rows = self
                         .notionrs_client
                         .get_block_children_all()
@@ -461,7 +463,7 @@ impl Client {
 
                     if let Some((header_row, body_rows)) = rows.split_first() {
                         let table_header_block =
-                            if let notionrs::block::Block::TableRow { table_row } =
+                            if let notionrs::object::block::Block::TableRow { table_row } =
                                 &header_row.block
                             {
                                 let cells_blocks = table_row
@@ -498,7 +500,7 @@ impl Client {
 
                         let table_body_row_blocks =
                             body_rows.iter().filter_map(|row| match &row.block {
-                                notionrs::block::Block::TableRow { table_row } => {
+                                notionrs::object::block::Block::TableRow { table_row } => {
                                     let cells_blocks = table_row
                                         .cells
                                         .iter()
@@ -538,9 +540,9 @@ impl Client {
                         blocks.push(table_block);
                     }
                 }
-                notionrs::block::Block::TableRow { table_row: _ } => {}
-                notionrs::block::Block::Template { template: _ } => {}
-                notionrs::block::Block::ToDo { to_do } => {
+                notionrs::object::block::Block::TableRow { table_row: _ } => {}
+                notionrs::object::block::Block::Template { template: _ } => {}
+                notionrs::object::block::Block::ToDo { to_do } => {
                     let props = crate::block::ElmCheckboxProps {
                         label: to_do
                             .rich_text
@@ -554,7 +556,7 @@ impl Client {
 
                     blocks.push(block);
                 }
-                notionrs::block::Block::Toggle { toggle } => {
+                notionrs::object::block::Block::Toggle { toggle } => {
                     let summary = toggle
                         .rich_text
                         .iter()
@@ -573,28 +575,30 @@ impl Client {
 
                     blocks.push(block);
                 }
-                notionrs::block::Block::Video { video: _ } => {}
-                notionrs::block::Block::Unknown(_) => {}
+                notionrs::object::block::Block::Video { video: _ } => {}
+                notionrs::object::block::Block::Unknown(_) => {}
             };
         }
 
         Ok(blocks)
     }
 
-    pub fn convert_rich_text(rich_text: Vec<notionrs::RichText>) -> Vec<crate::block::Block> {
+    pub fn convert_rich_text(
+        rich_text: Vec<notionrs::object::rich_text::RichText>,
+    ) -> Vec<crate::block::Block> {
         let mut blocks: Vec<crate::block::Block> = Vec::new();
 
         for r in rich_text {
             let annotations = match r {
-                notionrs::RichText::Text { annotations, .. } => annotations,
-                notionrs::RichText::Mention { annotations, .. } => annotations,
-                notionrs::RichText::Equation { annotations, .. } => annotations,
+                notionrs::object::rich_text::RichText::Text { annotations, .. } => annotations,
+                notionrs::object::rich_text::RichText::Mention { annotations, .. } => annotations,
+                notionrs::object::rich_text::RichText::Equation { annotations, .. } => annotations,
             };
 
             let plain_text = match r {
-                notionrs::RichText::Text { plain_text, .. } => plain_text,
-                notionrs::RichText::Mention { plain_text, .. } => plain_text,
-                notionrs::RichText::Equation { plain_text, .. } => plain_text,
+                notionrs::object::rich_text::RichText::Text { plain_text, .. } => plain_text,
+                notionrs::object::rich_text::RichText::Mention { plain_text, .. } => plain_text,
+                notionrs::object::rich_text::RichText::Equation { plain_text, .. } => plain_text,
             };
 
             let props = crate::block::ElmInlineTextProps {
@@ -605,16 +609,16 @@ impl Client {
                 strikethrough: annotations.strikethrough,
                 code: annotations.code,
                 color: match annotations.color {
-                    notionrs::Color::Default => None,
-                    notionrs::Color::Blue => Some(String::from("#6987b8")),
-                    notionrs::Color::Brown => Some(String::from("#8b4c3f")),
-                    notionrs::Color::Gray => Some(String::from("#868e9c")),
-                    notionrs::Color::Green => Some(String::from("#59b57c")),
-                    notionrs::Color::Orange => Some(String::from("#bf7e71")),
-                    notionrs::Color::Pink => Some(String::from("#c9699e")),
-                    notionrs::Color::Purple => Some(String::from("#9771bd")),
-                    notionrs::Color::Red => Some(String::from("#b36472")),
-                    notionrs::Color::Yellow => Some(String::from("#b8a36e")),
+                    notionrs::object::color::Color::Default => None,
+                    notionrs::object::color::Color::Blue => Some(String::from("#6987b8")),
+                    notionrs::object::color::Color::Brown => Some(String::from("#8b4c3f")),
+                    notionrs::object::color::Color::Gray => Some(String::from("#868e9c")),
+                    notionrs::object::color::Color::Green => Some(String::from("#59b57c")),
+                    notionrs::object::color::Color::Orange => Some(String::from("#bf7e71")),
+                    notionrs::object::color::Color::Pink => Some(String::from("#c9699e")),
+                    notionrs::object::color::Color::Purple => Some(String::from("#9771bd")),
+                    notionrs::object::color::Color::Red => Some(String::from("#b36472")),
+                    notionrs::object::color::Color::Yellow => Some(String::from("#b8a36e")),
                     _ => None,
                 },
             };
