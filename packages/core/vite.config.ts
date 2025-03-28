c1d4cf5a71b9b4ecb4c190cfc8b90958953113a8
Chomolungma Shirayuki <shirayuki@46ki75.com>
2025-03-28 11:46:13 +0000

    chore: bump version to 1.0.0-alpha.152 and simplify Vite plugins configuration

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -2,18 +2,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import dts from 'vite-plugin-dts'
-import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    dts({ tsconfigPath: './tsconfig.app.json' }),
-    cssInjectedByJsPlugin()
-  ],
+  plugins: [vue(), vueJsx(), dts({ tsconfigPath: './tsconfig.app.json' })],
   build: {
     minify: false,
     lib: {
