d494e45473c9c9e65f927ac3f6cf6192610eeaac
Chomolungma Shirayuki <shirayuki@46ki75.com>
2025-03-28 11:56:00 +0000

    chore: bump version to 1.0.0-alpha.153 and add css-injected-by-js plugin to Vite configuration

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -2,12 +2,18 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import dts from 'vite-plugin-dts'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), dts({ tsconfigPath: './tsconfig.app.json' })],
+  plugins: [
+    vue(),
+    vueJsx(),
+    dts({ tsconfigPath: './tsconfig.app.json' }),
+    cssInjectedByJsPlugin()
+  ],
   build: {
     minify: false,
     lib: {
