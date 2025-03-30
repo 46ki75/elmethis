94cff42cb72afc1f01e953568db486374d71ea25
Chomolungma Shirayuki <shirayuki@46ki75.com>
2025-03-30 06:51:29 +0000

    feat: add ElmInlineIcon component with Storybook documentation

---
+++ packages/core/src/components/inline/ElmInlineIcon.stories.tsx
@@ -0,0 +1,14 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+import ElmInlineIcon from './ElmInlineIcon.vue'
+
+const meta: Meta<typeof ElmInlineIcon> = {
+  title: 'Components/Inline/ElmInlineIcon',
+  component: ElmInlineIcon,
+  tags: ['autodocs'],
+  args: {}
+}
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {}
---
+++ packages/core/src/components/inline/ElmInlineIcon.vue
@@ -0,0 +1,21 @@
+<template>
+  <img :class="$style.icon" :src="src" :alt="alt" />
+</template>
+
+<script setup lang="ts">
+export interface ElmInlineIconProps {
+  // URL of the icon.
+  src: string
+
+  alt?: string
+}
+
+withDefaults(defineProps<ElmInlineIconProps>(), {})
+</script>
+
+<style module lang="scss">
+.icon {
+  display: inline;
+  height: 1em;
+}
+</style>
