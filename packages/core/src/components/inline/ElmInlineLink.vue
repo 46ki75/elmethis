933c461a44f75a50871bbe96bf5672447ec75cbc
Chomolungma Shirayuki <shirayuki@46ki75.com>
2025-04-02 22:21:28 +0000

    feat: add favicon support to ElmInlineLink component

--- packages/core/src/components/inline/ElmInlineLink.vue
+++ packages/core/src/components/inline/ElmInlineLink.vue
@@ -7,7 +7,13 @@
     rel="noopener noreferrer"
     @click="handleClick"
   >
-    <img :src="src" alt="favicon" @error="handleError" :class="$style.icon" />
+    <img
+      v-if="favicon"
+      :src="favicon"
+      alt="favicon"
+      @error="handleError"
+      :class="$style.icon"
+    />
 
     {{ text ?? href }}
     <Icon
@@ -42,6 +48,8 @@ export interface ElmInlineLinkProps {
    * e.g. `https://example.com`
    */
   href: string
+
+  favicon?: string
 
   /**
    * Specifies the font size of the text.
@@ -78,8 +86,6 @@ function handleClick(event: MouseEvent) {
     props.onClick()
   }
 }
-
-const src = ref(`https://logo.clearbit.com/${new URL(props.href!).hostname}`)
 
 const handleError = () => {
   src.value = `data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="gray" d="M17.9 17.39c-.26-.8-1.01-1.39-1.9-1.39h-1v-3a1 1 0 0 0-1-1H8v-2h2a1 1 0 0 0 1-1V7h2a2 2 0 0 0 2-2v-.41a7.984 7.984 0 0 1 2.9 12.8M11 19.93c-3.95-.49-7-3.85-7-7.93c0-.62.08-1.22.21-1.79L9 15v1a2 2 0 0 0 2 2m1-16A10 10 0 0 0 2 12a10 10 0 0 0 10 10a10 10 0 0 0 10-10A10 10 0 0 0 12 2"/></svg>`
