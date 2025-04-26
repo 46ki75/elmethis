7c18230a983b0a53d27f3597bd5f6feee2f1b076
Chomolungma Shirayuki <shirayuki@46ki75.com>
2025-04-26 06:13:22 +0000

    refactor: reorganize component types and interfaces for clarity and consistency

--- packages/json-component-types/src/index.ts
+++ packages/json-component-types/src/index.ts
@@ -1,10 +1,21 @@
-export type ComponentType =
-  | 'Text'
-  | 'Icon'
+export type InlineComponentType = 'Text' | 'Icon'
+
+export type BlockComponentType =
+  | 'Heading'
   | 'Paragraph'
   | 'ListItem'
-  | 'BulletedList'
-  | 'NumberedLists'
+  | 'List'
+  | 'BlockQuote'
+  | 'Callout'
+  | 'Divider'
+  | 'Toggle'
+  | 'Bookmark'
+  | 'File'
+  | 'Image'
+  | 'CodeBlock'
+  | 'Katex'
+
+export type ComponentType = InlineComponentType | BlockComponentType
 
 export interface Component<P = Record<any, any>> {
   type: ComponentType
@@ -12,7 +23,21 @@ export interface Component<P = Record<any, any>> {
   slots?: Record<string, Component[]>
 }
 
-export interface Text extends Component {
+export interface InlineComponent<P = Record<any, any>> extends Component {
+  type: ComponentType
+  props?: P
+  slots: undefined
+}
+
+export interface BlockComponent<P = Record<any, any>> extends Component {
+  type: ComponentType
+  props?: P
+  slots?: Record<string, Component[]>
+}
+
+// Inline Components # -------------------------------------------------- #
+
+export interface Text extends InlineComponent {
   type: 'Text'
   props: {
     text: string
@@ -30,10 +55,15 @@ export interface Text extends Component {
 
     href?: string
     favicon?: string
+
+    /**
+     * KaTex expression.
+     */
+    expression?: string
   }
 }
 
-export interface Icon extends Component {
+export interface Icon extends InlineComponent {
   type: 'Icon'
   props: {
     src: string
@@ -41,22 +71,99 @@ export interface Icon extends Component {
   }
 }
 
-export interface Paragraph extends Component {
+// Block Components # -------------------------------------------------- #
+
+export interface Heading extends BlockComponent {
+  type: 'Heading'
+  props: {
+    level: 1 | 2 | 3 | 4 | 5 | 6
+  }
+  slots: { default: InlineComponent[] }
+}
+
+export interface Paragraph extends BlockComponent {
   type: 'Paragraph'
   slots: { default: Component[] }
 }
 
-export interface ListItem extends Component {
+export interface ListItem extends BlockComponent {
   type: 'ListItem'
   slots: { default: Component[] }
 }
 
-export interface BulletedList extends Component {
-  type: 'BulletedList'
+export interface List extends BlockComponent {
+  type: 'List'
+  props?: { listStyle?: 'unordered' | 'ordered' }
   slots: { default: Component[] }
 }
 
-export interface NumberedLists extends Component {
-  type: 'NumberedLists'
+export interface BlockQuote extends BlockComponent {
+  type: 'BlockQuote'
+  props?: { cite?: string }
   slots: { default: Component[] }
+}
+
+export interface Callout extends BlockComponent {
+  type: 'Callout'
+  props?: { type?: 'note' | 'tip' | 'important' | 'warning' | 'caution' }
+  slots: { default: Component[] }
+}
+
+export interface Divider extends BlockComponent {
+  type: 'Divider'
+}
+
+export interface Toggle extends BlockComponent {
+  type: 'Toggle'
+  slots: {
+    default: Component[]
+    summary: InlineComponent[]
+  }
+}
+
+export interface Bookmark extends BlockComponent {
+  type: 'Bookmark'
+  props: {
+    url: string
+    title?: string
+    description?: string
+    image?: string
+  }
+}
+
+export interface File extends BlockComponent {
+  type: 'File'
+  props: {
+    src: string
+    name?: string
+  }
+}
+
+export interface Image extends BlockComponent {
+  type: 'Image'
+  props: {
+    src: string
+    alt?: string
+  }
+}
+
+export interface CodeBlock extends BlockComponent {
+  type: 'CodeBlock'
+  props: {
+    code: string
+    language: string
+  }
+  slots: {
+    /**
+     * Caption of the code.
+     */
+    default: InlineComponent[]
+  }
+}
+
+export interface Katex extends BlockComponent {
+  type: 'Katex'
+  props: {
+    expression: string
+  }
 }
