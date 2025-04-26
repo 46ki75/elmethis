c78c40d78a972dfbc1cba3aace0f8fce507186d2
Chomolungma Shirayuki <shirayuki@46ki75.com>
2025-04-26 13:39:59 +0000

    feat: add mdi icons to ElmBlockQuote component and update dependencies

--- packages/core/src/components/typography/ElmBlockQuote.vue
+++ packages/core/src/components/typography/ElmBlockQuote.vue
@@ -5,16 +5,40 @@
     :style="{
       '--opacity': targetIsVisible ? 1 : 0
     }"
+    :cite="cite"
   >
     <slot />
+
+    <svg
+      :class="$style.icon"
+      style="top: 0.25rem; left: 0.25rem"
+      viewBox="0 0 24 24"
+      width="1rem"
+      height="1rem"
+    >
+      <path :d="mdiFormatQuoteOpen" />
+    </svg>
+
+    <svg
+      :class="$style.icon"
+      style="bottom: 0.25rem; right: 0.25rem"
+      viewBox="0 0 24 24"
+      width="1rem"
+      height="1rem"
+    >
+      <path :d="mdiFormatQuoteClose" />
+    </svg>
   </blockquote>
 </template>
 
 <script setup lang="ts">
 import { useIntersectionObserver } from '@vueuse/core'
 import { ref } from 'vue'
+import { mdiFormatQuoteOpen, mdiFormatQuoteClose } from '@mdi/js'
 
-export interface ElmBlockQuoteProps {}
+export interface ElmBlockQuoteProps {
+  cite?: string
+}
 
 withDefaults(defineProps<ElmBlockQuoteProps>(), {})
 
@@ -28,16 +52,28 @@ useIntersectionObserver(target, ([{ isIntersecting }], _) => {
 
 <style module lang="scss">
 .blockquote {
+  position: relative;
   margin-block: 2rem;
   opacity: var(--opacity);
   transition: opacity 800ms;
   box-sizing: border-box;
   margin: 0;
-  padding: 0.125rem 1.5rem;
+  padding: 0.75rem 0.5rem 0.75rem 1.5rem;
+  background-color: rgba(#868e9c, 0.05);
+
   border-left: 4px solid rgba(black, 0.2);
 
   [data-theme='dark'] & {
     border-left-color: rgba(white, 0.2);
   }
+}
+
+.icon {
+  position: absolute;
+  padding: 0;
+  margin: 0;
+  width: 1rem;
+  height: 1rem;
+  fill: #868e9c;
 }
 </style>
