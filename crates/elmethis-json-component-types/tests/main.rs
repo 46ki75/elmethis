5f2cf0d8dedca73906630cf98409b4517d70721e
Chomolungma Shirayuki <shirayuki@46ki75.com>
2025-04-27 07:07:17 +0000

    test: rename main function to serialize and update test to serialize paragraph component

--- crates/elmethis-json-component-types/tests/main.rs
+++ crates/elmethis-json-component-types/tests/main.rs
@@ -1,6 +1,8 @@
+use elmethis_json_component_types::ParagraphSlots;
+
 #[test]
-fn main() {
-    let inline = elmethis_json_component_types::Component::InlineComponent(
+fn serialize() {
+    let inline =
         elmethis_json_component_types::InlineComponent::Text(elmethis_json_component_types::Text {
             r#type: "Text".to_string(),
             inline: true,
@@ -9,8 +11,20 @@ fn main() {
                 ..Default::default()
             },
             ..Default::default()
-        }),
+        });
+
+    let paragraph = elmethis_json_component_types::Component::BlockComponent(
+        elmethis_json_component_types::BlockComponent::Paragraph(
+            elmethis_json_component_types::Paragraph {
+                r#type: "Paragraph".to_string(),
+                inline: false,
+                props: None,
+                slots: ParagraphSlots {
+                    default: vec![inline],
+                },
+            },
+        ),
     );
 
-    println!("{}", serde_json::to_string(&inline).unwrap());
+    println!("{}", serde_json::to_string(&paragraph).unwrap());
 }
