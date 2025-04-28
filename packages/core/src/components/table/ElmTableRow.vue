0e75d35c458c5714606f8c5d31a16e7cd48b8e4d
Chomolungma Shirayuki <shirayuki@46ki75.com>
2025-04-28 15:43:34 +0000

    refactor(table): enhance ElmTableRow with conditional row header styling

--- packages/core/src/components/table/ElmTableRow.vue
+++ packages/core/src/components/table/ElmTableRow.vue
@@ -1,5 +1,5 @@
 <template>
-  <tr :class="$style.tr">
+  <tr :class="[$style.tr, { [$style['has-row-header']]: hasRowHeader }]">
     <template v-for="cell in slots">
       <component :is="cell" />
     </template>
@@ -7,6 +7,7 @@
 </template>
 
 <script setup lang="ts">
+import { inject } from "vue";
 import ElmTableCell from "./ElmTableCell.vue";
 
 export interface ElmTableRowProps {}
@@ -16,6 +17,8 @@ withDefaults(defineProps<ElmTableRowProps>(), {});
 const slots = defineSlots<{
   default: InstanceType<typeof ElmTableCell>[];
 }>();
+
+const hasRowHeader = inject<boolean>("hasRowHeader");
 </script>
 
 <style module lang="scss">
@@ -27,9 +30,9 @@ const slots = defineSlots<{
   }
 
   &:nth-child(even) {
-    background-color: rgba(black, 0.025);
+    background-color: rgba(black, 0.015);
     [data-theme="dark"] & {
-      background-color: rgba(white, 0.025);
+      background-color: rgba(white, 0.015);
     }
   }
 
@@ -38,6 +41,13 @@ const slots = defineSlots<{
     [data-theme="dark"] & {
       background-color: rgba(#6987b8, 0.15);
     }
+  }
+}
+
+.has-row-header {
+  td:first-child,
+  tr:first-child {
+    background-color: rgba(gray, 0.15);
   }
 }
 </style>
