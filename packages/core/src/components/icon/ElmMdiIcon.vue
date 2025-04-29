dfc7c2ae38dfea6c67bc23c1e4c4a71a6e0d6dfe
Chomolungma Shirayuki <shirayuki@46ki75.com>
2025-04-29 07:11:22 +0000

    feat(icon): enhance ElmMdiIcon component with accessibility attributes and dynamic styling

--- packages/core/src/components/icon/ElmMdiIcon.vue
+++ packages/core/src/components/icon/ElmMdiIcon.vue
@@ -1,6 +1,17 @@
 <template>
-  <svg viewBox="0 0 24 24" :width="size" :height="size">
-    <path :d="d" fill="currentColor" />
+  <svg
+    viewBox="0 0 24 24"
+    :width="size"
+    :height="size"
+    focusable="false"
+    role="img"
+    :class="$style.icon"
+    :style="{
+      '--color': lightColor ?? color,
+      '--dark-color': darkColor ?? color,
+    }"
+  >
+    <path :d="d" />
   </svg>
 </template>
 
@@ -8,6 +19,9 @@
 export interface ElmMdiIconProps {
   size?: string;
   d: string;
+  color?: string;
+  lightColor?: string;
+  darkColor?: string;
 }
 
 withDefaults(defineProps<ElmMdiIconProps>(), {
@@ -15,4 +29,12 @@ withDefaults(defineProps<ElmMdiIconProps>(), {
 });
 </script>
 
-<style module lang="scss"></style>
+<style module lang="scss">
+.icon {
+  fill: var(--color, #333);
+
+  [data-theme="dark"] & {
+    fill: var(--dark-color, #ccc);
+  }
+}
+</style>
