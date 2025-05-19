e886a4a4491c15d74e9a5f3441fb184155bcb319
Chomolungma Shirayuki <shirayuki@46ki75.com>
2025-05-19 10:11:15 +0000

    refactor: remove ElmBlockImage component and related stories; update ElmImage component to support modal and block display

---
+++ packages/core/src/components/media/ElmImageModal.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from "@storybook/vue3";
+import ElmImageModal from "./ElmImageModal.vue";
+
+const meta: Meta<typeof ElmImageModal> = {
+  title: "Components/Media/ElmImageModal",
+  component: ElmImageModal,
+  tags: ["autodocs"],
+  args: {
+    src: "https://images.unsplash.com/photo-1556983703-27576e5afa24?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb",
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {};
+
+export const Svg: Story = {
+  args: {
+    src: "https://nuxt.com/cdn-cgi/image/w=1024,h=878/assets/landing/deploy.svg",
+  },
+};
---
+++ packages/core/src/components/media/ElmImageModal.vue
@@ -0,0 +1,42 @@
+<template>
+  <div :class="$style.modal">
+    <img :class="$style.image" :src="src" :alt="alt" />
+  </div>
+</template>
+
+<script setup lang="ts">
+export interface ElmImageModalProps {
+  /**
+   * Image source URL
+   */
+  src: string;
+
+  /**
+   * Image alt text
+   */
+  alt?: string;
+}
+
+withDefaults(defineProps<ElmImageModalProps>(), {});
+</script>
+
+<style module lang="scss">
+.modal {
+  position: fixed;
+  z-index: 10;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(#23262a, 0.8);
+  cursor: zoom-out;
+}
+
+.image {
+  display: block;
+  max-width: 100%;
+}
+</style>
