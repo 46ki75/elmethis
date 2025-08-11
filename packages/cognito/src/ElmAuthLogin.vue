85d0a4cd26a279ec815f334978d233d5122e6ab2
Ikuma Yamashita <me@ikuma.cloud>
2025-08-11 21:23:41 +0900

    feat: initialize package

---
+++ packages/cognito/src/ElmAuthLogin.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Template</div>
+</template>
+
+<script setup lang="ts">
+export interface ElmAuthLoginProps {}
+
+withDefaults(defineProps<ElmAuthLoginProps>(), {});
+</script>
+
+<style module lang="scss"></style>
---
+++ packages/cognito/src/index.ts
@@ -0,0 +1,4 @@
+export {
+  default as ElmAuthLogin,
+  type ElmAuthLoginProps as ElmCognitoProps,
+} from "./ElmAuthLogin.vue";
