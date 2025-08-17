6d58cb757c97fd5ccd10ff0558bba95859aee04e
Ikuma Yamashita <me@ikuma.cloud>
2025-08-17 19:35:30 +0900

    Revert "Merge pull request #844 from 46ki75/fix/image-cursor"
    This reverts commit 7211846cfb2cc25a4cc2af1be3e24a555e913b5e, reversing
    changes made to 96abf992b9421eb72deabfd89e5de42264c84d25.

--- packages/core/src/components/media/ElmImage.vue
+++ packages/core/src/components/media/ElmImage.vue
@@ -35,7 +35,7 @@
         :alt="alt"
         @click="if (enableModal) isModalOpen = true;"
         :style="{
-          '--cursor': enableModal ? 'zoom-in' : 'inherit',
+          '--cursor': enableModal ? 'zoom-in' : 'default',
           '--margin-block': margin,
         }"
       />
@@ -158,7 +158,7 @@ useIntersectionObserver(target, ([{ isIntersecting }], _) => {
   margin-block: var(--margin-block);
   max-width: 100%;
   max-height: 100vh;
-  cursor: var(--cursor, inherit);
+  cursor: var(--cursor, default);
 }
 
 .error {
