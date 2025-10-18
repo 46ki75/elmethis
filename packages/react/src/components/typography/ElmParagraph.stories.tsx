ca2ee5c366fa0fd664457c19d3b6a930c16cfe5c
Ikuma Yamashita <me@ikuma.cloud>
2025-10-18 15:38:08 +0000

    feat: add ElmParagraph component and associated styles

---
+++ packages/react/src/components/typography/ElmParagraph.stories.tsx
@@ -0,0 +1,14 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import ElmParagraph from "./ElmParagraph";
+
+const meta: Meta<typeof ElmParagraph> = {
+  title: "Components/Typography/ElmParagraph",
+  component: ElmParagraph,
+  tags: ["autodocs"],
+  args: {},
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {};
---
+++ packages/react/src/components/typography/ElmParagraph.tsx
@@ -0,0 +1,7 @@
+import textStyle from "../../styles/text.module.scss";
+
+const ElmParagraph = () => {
+  return <p className={textStyle.text}>ElmParagraph</p>;
+};
+
+export default ElmParagraph;
