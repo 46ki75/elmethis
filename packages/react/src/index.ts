72b9e677cb8444c5b57d256144da7bfa3eb43a6c
Ikuma Yamashita <me@ikuma.cloud>
2025-10-19 02:41:27 +0000

    feat: add ElmMdiIcon export to index.ts for improved accessibility

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -3,6 +3,7 @@ export {
   ElmInlineIcon,
   type ElmInlineIconProps,
 } from "./components/icon/ElmInlineIcon";
+export { ElmMdiIcon, type ElmMdiIconProps } from "./components/icon/ElmMdiIcon";
 
 // media
 export { ElmImage, type ElmImageProps } from "./components/media/ElmImage";
