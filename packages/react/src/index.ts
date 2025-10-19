57c469fbe7794c1007956bda5b600c4e646ffba0
Ikuma Yamashita <me@ikuma.cloud>
2025-10-19 02:37:15 +0000

    feat: reorganize exports in index.ts for better structure and clarity

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -4,7 +4,18 @@ export {
   type ElmInlineIconProps,
 } from "./components/icon/ElmInlineIcon";
 
+// media
+export { ElmImage, type ElmImageProps } from "./components/media/ElmImage";
+
 // typography
+export {
+  ElmBlockQuote,
+  type ElmBlockQuoteProps,
+} from "./components/typography/ElmBlockQuote";
+export {
+  ElmCallout,
+  type ElmCalloutProps,
+} from "./components/typography/ElmCallout";
 export {
   ElmInlineText,
   type ElmInlineTextProps,
