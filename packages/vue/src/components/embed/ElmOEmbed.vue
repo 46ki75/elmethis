7d58a917b1c84af605e43fb677811779d175f8e1
Ikuma Yamashita <me@ikuma.cloud>
2026-02-24 04:13:17 +0900

    feat: add ElmOEmbed component with support for photo, video, rich, and link types

---
+++ packages/vue/src/components/embed/ElmOEmbed.vue
@@ -0,0 +1,87 @@
+<template>
+  <div>
+    <ElmImage
+      v-if="oEmbed.type === 'photo'"
+      :src="oEmbed.url"
+      :alt="oEmbed.title"
+      :width="oEmbed.width"
+      :height="oEmbed.height"
+      block
+    />
+
+    <div v-else-if="oEmbed.type === 'video'">
+      <div
+        v-html="oEmbed.html"
+        :style="{
+          width: oEmbed.width && `${oEmbed.width}px`,
+          height: oEmbed.height && `${oEmbed.height}px`,
+        }"
+      />
+    </div>
+
+    <div
+      v-else-if="oEmbed.type === 'rich'"
+      :style="{
+        width: oEmbed.width && `${oEmbed.width}px`,
+        height: oEmbed.height && `${oEmbed.height}px`,
+      }"
+    >
+      <div v-html="oEmbed.html" />
+    </div>
+
+    <div v-else-if="oEmbed.type === 'link'"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import ElmImage from "../media/ElmImage.vue";
+
+interface OEmbedBase {
+  type: "photo" | "video" | "link" | "rich";
+  title?: string;
+  author_name?: string;
+  author_url?: string;
+  provider_name?: string;
+  provider_url?: string;
+  cache_age?: number;
+  thumbnail_url?: string;
+  thumbnail_width?: number;
+  thumbnail_height?: number;
+  [key: string]: any;
+}
+
+interface OEmbedPhoto extends OEmbedBase {
+  type: "photo";
+  url: string;
+  width: number;
+  height: number;
+}
+
+interface OEmbedVideo extends OEmbedBase {
+  type: "video";
+  html: string;
+  width: number;
+  height: number;
+}
+
+interface OEmbedRich extends OEmbedBase {
+  type: "rich";
+  html: string;
+  width: number;
+  height: number;
+}
+
+interface OEmbedLink extends OEmbedBase {
+  type: "link";
+}
+
+type OEmbedResponse = OEmbedPhoto | OEmbedVideo | OEmbedRich | OEmbedLink;
+
+export interface ElmOEmbedProps {
+  oEmbed: OEmbedResponse;
+}
+
+withDefaults(defineProps<ElmOEmbedProps>(), {});
+</script>
+
+<style module lang="scss"></style>
