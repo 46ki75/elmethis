201dc3c523e60b1756c6c94e489bc54087a85cc8
Ikuma Yamashita <me@ikuma.cloud>
2026-02-28 14:15:40 +0900

    feat: reorganize exports in index.ts for better structure

--- packages/qwik/src/index.ts
+++ packages/qwik/src/index.ts
@@ -1,4 +1,26 @@
+// | Icon |
 export {
   ElmDotLoadingIcon,
   type ElmDotLoadingIconProps,
 } from "./components/icon/elm-dot-loading-icon";
+
+export {
+  ElmInlineIcon,
+  type ElmInlineIconProps,
+} from "./components/icon/elm-inline-icon";
+
+export {
+  ElmLanguageIcon,
+  type ElmLanguageIconProps,
+} from "./components/icon/elm-language-icon";
+
+export {
+  ElmMdiIcon,
+  type ElmMdiIconProps,
+} from "./components/icon/elm-mdi-icon";
+
+// | Typography |
+export {
+  ElmInlineText,
+  type ElmInlineTextProps,
+} from "./components/typography/elm-inline-text";
