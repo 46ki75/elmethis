b6ef0918b9d26091b393b17c990c69fdc3e79dd7
Ikuma Yamashita <me@ikuma.cloud>
2026-02-28 13:41:11 +0900

    feat: refactor Storybook decorator to use Component type and simplify return structure

--- packages/qwik/.storybook/preview.tsx
+++ packages/qwik/.storybook/preview.tsx
@@ -1,6 +1,7 @@
 import { Parameters, StoryContext } from "storybook-framework-qwik";
 
 import "./sb.scss";
+import { Component } from "@builder.io/qwik";
 
 export const parameters: Parameters = {
   a11y: {
@@ -38,14 +39,11 @@ export const parameters: Parameters = {
   },
   initialGlobals: { theme: "light" },
   decorators: [
-    (story: () => unknown, context: StoryContext) => {
+    (Story: Component, context: StoryContext) => {
       const theme = context.globals.theme || "light";
       document.documentElement.setAttribute("data-theme", theme);
 
-      return {
-        components: { story },
-        template: `<story />`,
-      };
+      return <Story />;
     },
   ],
 };
