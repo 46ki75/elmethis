5157cb761283673a29b7fd497e057e2f2b6ae465
Ikuma Yamashita <me@ikuma.cloud>
2026-02-28 13:25:25 +0900

    feat: add ElmInlineText component with styles and storybook integration; include polished dependency

---
+++ packages/qwik/src/components/typography/elm-inline-text.stories.tsx
@@ -0,0 +1,54 @@
+import type { Meta, StoryObj } from "storybook-framework-qwik";
+import { ElmInlineText } from "./elm-inline-text";
+import { opacify } from "polished";
+
+const meta: Meta<typeof ElmInlineText> = {
+  title: "Components/Typography/elm-inline-text",
+  component: ElmInlineText,
+  tags: ["autodocs"],
+  argTypes: { color: { control: "color" } },
+  args: { text: "Inline Text" },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {};
+
+export const Colored: Story = {
+  args: { color: "#b36472" },
+};
+
+export const Kbd: Story = {
+  args: { text: "Ctrl", kbd: true },
+};
+
+export const Background: Story = {
+  args: { backgroundColor: opacify(-0.5, "#6987b8") },
+};
+
+export const Ruby: Story = {
+  args: { text: "国際連合", ruby: "こくさいれんごう" },
+};
+
+export const Link: Story = {
+  args: {
+    text: "Google Search",
+    href: "https://google.com",
+    favicon: "https://www.google.com/favicon.ico",
+  },
+};
+
+export const LinkWithOgp: Story = {
+  args: {
+    text: "Visual Studio Code - Code Editing. Redefined",
+    href: "https://code.visualstudio.com/",
+    favicon: "https://code.visualstudio.com/assets/favicon.ico",
+    ogp: {
+      title: "Visual Studio Code - Code Editing. Redefined",
+      description:
+        "Visual Studio Code redefines AI-powered coding with GitHub Copilot for building and debugging modern web and cloud applications. Visual Studio Code is free and available on your favorite platform - Linux, macOS, and Windows.",
+      image: "https://code.visualstudio.com/opengraphimg/opengraph-home.png",
+    },
+  },
+};
