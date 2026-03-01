3876bfaf8eba6d25afc3efa56558a0f89133cb19
Ikuma Yamashita <me@ikuma.cloud>
2026-03-01 19:16:43 +0900

    style: organize exports in index.ts by category for improved readability

--- packages/qwik/src/index.ts
+++ packages/qwik/src/index.ts
@@ -3,10 +3,27 @@ export {
   ElmCodeBlock,
   type ElmCodeBlockProps,
 } from "./components/code/elm-code-block";
+export { ElmKatex, type ElmKatexProps } from "./components/code/elm-katex";
 export {
   ElmShikiHighlighter,
   type ElmShikiHighlighterProps,
 } from "./components/code/elm-shiki-highlighter";
+
+// | Containments |
+export {
+  ElmToggle,
+  type ElmToggleProps,
+} from "./components/containments/elm-toggle";
+
+// | Fallback |
+export {
+  ElmBlockFallback,
+  type ElmBlockFallbackProps,
+} from "./components/fallback/elm-block-fallback";
+export {
+  ElmRectangleWave,
+  type ElmRectangleWaveProps,
+} from "./components/fallback/elm-rectangle-wave";
 
 // | Icon |
 export {
@@ -29,11 +46,49 @@ export {
   type ElmMdiIconProps,
 } from "./components/icon/elm-mdi-icon";
 
+// | Media |
+export {
+  ElmBlockImage,
+  type ElmBlockImageProps,
+} from "./components/media/elm-block-image";
+export { ElmFile, type ElmFileProps } from "./components/media/elm-file";
+
+// | Navigation |
+export {
+  ElmBookmark,
+  type ElmBookmarkProps,
+} from "./components/navigation/elm-bookmark";
+
 // | Typography |
+export {
+  ElmBlockQuote,
+  type ElmBlockQuoteProps,
+} from "./components/typography/elm-block-quote";
+export {
+  ElmCallout,
+  type ElmCalloutProps,
+} from "./components/typography/elm-callout";
+export {
+  ElmDivider,
+  type ElmDividerProps,
+} from "./components/typography/elm-divider";
+export {
+  ElmFragmentIdentifier,
+  type ElmFragmentIdentifierProps,
+} from "./components/typography/elm-fragment-identifier";
+export {
+  ElmHeading,
+  type ElmHeadingProps,
+} from "./components/typography/elm-heading";
 export {
   ElmInlineText,
   type ElmInlineTextProps,
 } from "./components/typography/elm-inline-text";
+export { ElmList, type ElmListProps } from "./components/typography/elm-list";
+export {
+  ElmParagraph,
+  type ElmParagraphProps,
+} from "./components/typography/elm-paragraph";
 
 // | Table |
 export { ElmTable, type ElmTableProps } from "./components/table/elm-table";
