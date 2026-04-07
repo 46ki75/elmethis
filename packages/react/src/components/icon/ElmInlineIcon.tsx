dcb4ddab13bd3afcd8de784fb44ed2bf3e430736
Ikuma Yamashita <me@ikuma.cloud>
2026-04-08 02:26:17 +0900

    chore!: remove unused files

--- packages/react/src/components/icon/ElmInlineIcon.tsx
+++
@@ -1,9 +0,0 @@
-import { type ComponentProps } from "react";
-
-import style from "./ElmInlineIcon.module.scss";
-
-export type ElmInlineIconProps = ComponentProps<"img">;
-
-export const ElmInlineIcon = (props: ElmInlineIconProps) => {
-  return <img {...props} className={style.icon} />;
-};
