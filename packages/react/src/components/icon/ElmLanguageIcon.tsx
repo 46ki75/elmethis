5d0b8bbaff564fa5741080cc684affcf0d63f49c
Ikuma Yamashita <me@ikuma.cloud>
2026-04-11 16:08:11 +0900

    feat: Add SVG icons for various programming languages in the language-icon component
    - Added CSS icon (css.svg)
    - Added Go icon (go.svg)
    - Added HTML icon (html.svg)
    - Added Java icon (java.svg)
    - Added JSON icon (json.svg)
    - Added Kotlin icon (kotlin.svg)
    - Added Lua icon (lua.svg)
    - Added NPM icon (npm.svg)
    - Added Python icon (python.svg)
    - Added SQL icon (sql.svg)
    - Added Terraform icon (terraform.svg)
    - Added TypeScript copy icon (typescript_copy.svg)

--- packages/react/src/components/icon/ElmLanguageIcon.tsx
+++ packages/react/src/components/icon/ElmLanguageIcon.tsx
@@ -8,6 +8,21 @@ import styles from "./ElmLanguageIcon.module.css";
 import RustIcon from "./language-icon/rust.svg?url";
 import JavaScriptIcon from "./language-icon/javascript.svg?url";
 import TypeScriptIcon from "./language-icon/typescript.svg?url";
+import BashIcon from "./language-icon/bash.svg?url";
+import TerraformIcon from "./language-icon/terraform.svg?url";
+import HtmlIcon from "./language-icon/html.svg?url";
+import CssIcon from "./language-icon/css.svg?url";
+import NpmIcon from "./language-icon/npm.svg?url";
+import JavaIcon from "./language-icon/java.svg?url";
+import KotlinIcon from "./language-icon/kotlin.svg?url";
+import GoIcon from "./language-icon/go.svg?url";
+import PythonIcon from "./language-icon/python.svg?url";
+import SqlIcon from "./language-icon/sql.svg?url";
+import JsonIcon from "./language-icon/json.svg?url";
+import LuaIcon from "./language-icon/lua.svg?url";
+import CSharpIcon from "./language-icon/csharp.svg?url";
+import CppIcon from "./language-icon/cplusplus.svg?url";
+import CIcon from "./language-icon/c.svg?url";
 
 import MdiCodeIcon from "./language-icon/mdi-code.svg?url";
 
@@ -34,21 +49,21 @@ const LanguageIconMap: Record<Language, string> = {
   rust: RustIcon,
   javascript: JavaScriptIcon,
   typescript: TypeScriptIcon,
-  // shell: "devicon:bash",
-  // terraform: "devicon:terraform",
-  // html: "devicon:html5",
-  // css: "devicon:css",
-  // npm: "devicon:npm",
-  // java: "devicon:java",
-  // kotlin: "devicon:kotlin",
-  // go: "devicon:go",
-  // python: "devicon:python",
-  // sql: "devicon:mysql",
-  // json: "mdi:code-json",
-  // lua: "devicon:lua",
-  // csharp: "devicon:csharp",
-  // cpp: "devicon:cplusplus",
-  // c: "devicon:c",
+  shell: BashIcon,
+  terraform: TerraformIcon,
+  html: HtmlIcon,
+  css: CssIcon,
+  npm: NpmIcon,
+  java: JavaIcon,
+  kotlin: KotlinIcon,
+  go: GoIcon,
+  python: PythonIcon,
+  sql: SqlIcon,
+  json: JsonIcon,
+  lua: LuaIcon,
+  csharp: CSharpIcon,
+  cpp: CppIcon,
+  c: CIcon,
   file: MdiCodeIcon,
 };
 
