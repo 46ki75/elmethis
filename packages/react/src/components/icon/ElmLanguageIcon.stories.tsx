24e6ce454d4e3ce9def97a4c633e3e90ed416347
Ikuma Yamashita <me@ikuma.cloud>
2026-04-11 15:09:20 +0900

    feat: add support for additional icons in ElmLanguageIcon component and define language types

--- packages/react/src/components/icon/ElmLanguageIcon.stories.tsx
+++ packages/react/src/components/icon/ElmLanguageIcon.stories.tsx
@@ -1,11 +1,18 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
 import { ElmLanguageIcon } from "./ElmLanguageIcon";
+import { LANGUAGES } from "./language";
 
 const meta: Meta<typeof ElmLanguageIcon> = {
   title: "Components/Icon/ElmLanguageIcon",
   component: ElmLanguageIcon,
   tags: ["autodocs"],
   args: {},
+  argTypes: {
+    language: {
+      control: "radio",
+      options: LANGUAGES,
+    },
+  },
 };
 
 export default meta;
--- packages/react/src/components/icon/ElmLanguageIcon.tsx
+++ packages/react/src/components/icon/ElmLanguageIcon.tsx
@@ -1,16 +1,67 @@
 import React from "react";
 
+// Styles
 import "@styles/global.css";
 import styles from "./ElmLanguageIcon.module.css";
 
+// Icons
+import { addCollection, Icon } from "@iconify/react";
+import { icons as devIcons } from "@iconify-json/devicon";
+import { icons as mdiIcons } from "@iconify-json/mdi";
+
+// Types
+import type { Language } from "./language";
+
+addCollection(devIcons);
+addCollection(mdiIcons);
+
 export interface ElmLanguageIconProps {
-  style?: React.CSSProperties;
+  style?: React.CSSProperties & {
+    "--elmethis-size"?: number;
+  };
+
+  /**
+   * The size of the icon.
+   */
+  size?: number;
+
+  /**
+   * The language of the icon.
+   */
+  language: Language | string;
 }
+
+const LanguageIconMap: Record<Language, string> = {
+  rust: "devicon:rust",
+  javascript: "devicon:javascript",
+  typescript: "devicon:typescript",
+  shell: "devicon:bash",
+  terraform: "devicon:terraform",
+  html: "devicon:html5",
+  css: "devicon:css",
+  npm: "devicon:npm",
+  java: "devicon:java",
+  kotlin: "devicon:kotlin",
+  go: "devicon:go",
+  python: "devicon:python",
+  sql: "devicon:mysql",
+  json: "mdi:code-json",
+  lua: "devicon:lua",
+  csharp: "devicon:csharp",
+  cpp: "devicon:cplusplus",
+  c: "devicon:c",
+  file: "mdi:code",
+};
 
 export const ElmLanguageIcon = (props: ElmLanguageIconProps) => {
   return (
     <div className={styles["elm-language-icon"]} style={props.style}>
-      PLACEHOLDER
+      <Icon
+        icon={LanguageIconMap[props.language as Language] || "mdi:code"}
+        width={props.size}
+        height={props.size}
+        color="gray"
+      />
     </div>
   );
 };
