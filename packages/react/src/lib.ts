c0db7eb603b720c2bd77a77af54c985350261478
Ikuma Yamashita <me@ikuma.cloud>
2026-04-11 14:26:06 +0900

    refactor: reorganize exports in lib.ts for better structure

--- packages/react/src/lib.ts
+++ packages/react/src/lib.ts
@@ -1,5 +1,16 @@
-// Template
+// Icon
+export {
+  ElmInlineIcon,
+  type ElmInlineIconProps,
+} from "./components/icon/ElmInlineIcon";
 
+// Typography
+export {
+  ElmInlineText,
+  type ElmInlineTextProps,
+} from "./components/typography/ElmInlineText";
+
+// Template
 export {
   ElmTemplate,
   type ElmTemplateProps,
