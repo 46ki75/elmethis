f47e0c53b852cb9efe281f0d654b25b12453cf3b
Ikuma Yamashita <me@ikuma.cloud>
2026-04-13 03:01:29 +0900

    feat: update ToolApproval component with improved button layout and status indicators

--- packages/react/src/components/copilotkit/ToolApproval.tsx
+++ packages/react/src/components/copilotkit/ToolApproval.tsx
@@ -6,7 +6,7 @@ import "@styles/global.css";
 import styles from "./ToolApproval.module.css";
 
 // Components
-import { mdiHammerScrewdriver, mdiMinusCircle, mdiTools } from "@mdi/js";
+import { mdiCheckCircle, mdiHammerScrewdriver, mdiMinusCircle } from "@mdi/js";
 import { ElmButton } from "@components/form/ElmButton";
 import { ElmMdiIcon } from "@components/icon/ElmMdiIcon";
 import { ElmInlineText } from "@components/typography/ElmInlineText";
@@ -58,26 +58,42 @@ export const ToolApproval = (props: ToolApprovalProps) => {
         </ElmInlineText>
       </div>
 
-      <div className={styles["button-container"]}>
-        <ElmButton
-          onClick={props.onReject}
-          block
-          loading={props.status === ToolCallStatus.InProgress}
-          disabled={props.status === ToolCallStatus.Complete}
-        >
-          <ElmMdiIcon d={mdiMinusCircle} />
-          <ElmInlineText code>{props.rejectLabel || "Reject"}</ElmInlineText>
-        </ElmButton>
+      <div className={styles["in-progress-container"]}>
+        <ElmInlineText>Preparing...</ElmInlineText>
+      </div>
 
-        <ElmButton
-          onClick={props.onApprove}
-          block
-          loading={props.status === ToolCallStatus.InProgress}
-          disabled={props.status === ToolCallStatus.Complete}
-        >
-          <ElmMdiIcon d={mdiTools} />
-          <ElmInlineText code>{props.approveLabel || "Approve"}</ElmInlineText>
-        </ElmButton>
+      <div className={styles["executing-container"]}>
+        <div>
+          <ElmInlineText>
+            LLM requests your approval to execute the tool.
+          </ElmInlineText>
+        </div>
+
+        <div className={styles["button-container"]}>
+          <ElmButton
+            onClick={props.onReject}
+            block
+            loading={props.status === ToolCallStatus.InProgress}
+            disabled={props.status === ToolCallStatus.Complete}
+          >
+            <span className={styles["button-inner"]}>
+              <ElmMdiIcon d={mdiMinusCircle} />
+              <ElmInlineText>{props.rejectLabel || "Reject"}</ElmInlineText>
+            </span>
+          </ElmButton>
+
+          <ElmButton
+            onClick={props.onApprove}
+            block
+            loading={props.status === ToolCallStatus.InProgress}
+            disabled={props.status === ToolCallStatus.Complete}
+          >
+            <div className={styles["button-inner"]}>
+              <ElmMdiIcon d={mdiCheckCircle} />
+              <ElmInlineText>{props.approveLabel || "Approve"}</ElmInlineText>
+            </div>
+          </ElmButton>
+        </div>
       </div>
 
       <div className={styles["result-container"]}>{props.resultContent}</div>
