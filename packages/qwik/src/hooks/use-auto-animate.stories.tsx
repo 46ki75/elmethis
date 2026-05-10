aa5010fa517e84e52771d3c4ec08af91faf3cb88
Ikuma Yamashita <me@ikuma.cloud>
2026-05-11 08:07:51 +0900

    feat(qwik): add Grid story to useAutoAnimate with shuffle and rotate functionality

--- packages/qwik/src/hooks/use-auto-animate.stories.tsx
+++ packages/qwik/src/hooks/use-auto-animate.stories.tsx
@@ -1,7 +1,8 @@
 import type { Meta, StoryObj } from "storybook-framework-qwik";
 import { useAutoAnimate, type UseAutoAnimateOptions } from "./use-auto-animate";
-import { $, component$, useStore } from "@builder.io/qwik";
+import { $, component$, useStore, useStylesScoped$ } from "@builder.io/qwik";
 import { ElmList } from "../components/typography/elm-list";
+import { ElmInlineText } from "../components/typography/elm-inline-text";
 
 const meta: Meta<UseAutoAnimateOptions> = {
   title: "Hooks/useAutoAnimate",
@@ -33,6 +34,59 @@ export const List: Story = {
               <li key={item}>Item {item}</li>
             ))}
           </ElmList>
+        </div>
+      );
+    });
+
+    return <Render />;
+  },
+};
+
+export const Grid: Story = {
+  render: (options) => {
+    const Render = component$(() => {
+      const { ref } = useAutoAnimate(options);
+
+      const store = useStore<{ items: number[] }>({
+        items: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+      });
+
+      const handleSuffle = $(() => {
+        store.items = store.items.sort(() => Math.random() - 0.5);
+      });
+
+      const handleRotate = $(() => {
+        store.items = [...store.items.slice(1), store.items[0]];
+      });
+
+      useStylesScoped$(`
+          .grid {
+            display: grid;
+            grid-template-columns: repeat(4, 1fr);
+            grid-template-rows: repeat(3, 1fr);
+            gap: .25rem;
+            margin-top: 1rem;
+          }
+          .item {
+            background-color: white;
+            padding: 4rem 0;
+            text-align: center;
+            border: 1px solid oklch(from gray l c h / 25%);
+            border-radius: 0.25rem;
+          }
+        `);
+
+      return (
+        <div>
+          <button onClick$={handleSuffle}>Shuffle</button>
+          <button onClick$={handleRotate}>Rotate</button>
+          <div ref={ref} class="grid">
+            {store.items.map((item) => (
+              <div key={item} class="item">
+                <ElmInlineText>Item {item}</ElmInlineText>
+              </div>
+            ))}
+          </div>
         </div>
       );
     });
