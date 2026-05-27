c1ab726ca747bf06f33cee75ddf61e596449b58a
Ikuma Yamashita <me@ikuma.cloud>
2026-05-27 19:25:33 +0900

    style: update color variables in ElmToggle and ElmUnsupportedBlock for improved theme consistency

--- packages/qwik/src/components/fallback/elm-unsupported-block.tsx
+++ packages/qwik/src/components/fallback/elm-unsupported-block.tsx
@@ -19,11 +19,15 @@ export const ElmUnsupportedBlock = component$<ElmUnsupportedBlockProps>(
           >
             <path d="M13,9H11V7H13M13,17H11V11H13M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" />
           </svg>
-          <ElmInlineText color="var(--elmethis-color-accent-muted)">UNSUPPORTED BLOCK</ElmInlineText>
+          <ElmInlineText color="var(--elmethis-color-mono-common-fg-weak)">
+            UNSUPPORTED BLOCK
+          </ElmInlineText>
         </div>
         {details && (
           <div class={styles.details}>
-            <ElmInlineText color="var(--elmethis-color-accent-muted)">{details}</ElmInlineText>
+            <ElmInlineText color="var(--elmethis-color-mono-common-fg-weak)">
+              {details}
+            </ElmInlineText>
           </div>
         )}
       </div>
