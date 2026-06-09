34faad0be954cda173a436e11c4606ff413e5fc8
Ikuma Yamashita <me@ikuma.cloud>
2026-06-10 02:36:14 +0900

    refactor(table): update styles for table cells, headers, and rows

--- packages/qwik/src/components/table/elm-table.tsx
+++ packages/qwik/src/components/table/elm-table.tsx
@@ -39,12 +39,6 @@ export const ElmTable = component$<ElmTableProps>((props) => {
             <span class={styles.spacing}></span>
 
             <span class={styles["caption-inner"]}>
-              <svg viewBox="0 0 24 24" width="1rem" height="1rem">
-                <path
-                  d="M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4M12,6A6,6 0 0,0 6,12A6,6 0 0,0 12,18A6,6 0 0,0 18,12A6,6 0 0,0 12,6M12,8A4,4 0 0,1 16,12A4,4 0 0,1 12,16A4,4 0 0,1 8,12A4,4 0 0,1 12,8Z"
-                  fill="var(--elmethis-color-accent-info)"
-                />
-              </svg>
               <ElmInlineText>{caption}</ElmInlineText>
             </span>
 
